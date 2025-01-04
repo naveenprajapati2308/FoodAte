12edf281ff42e65356fbacc860a55f8661493be6
Naveen Prajapati <113762254+naveenprajapati2308@users.noreply.github.com>
2025-01-04 13:47:43 +0530

    Update Cart.jsx

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import './Cart.css'
+import './cart.css'
 import { StoreContext } from '../../context/StoreContext';
 import { useNavigate } from 'react-router-dom';
 
